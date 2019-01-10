d0705984fd3b7ebc0f33cfa6c2c6439d0d2017e5
Karen Pirumyan <k.pirumyan@gmail.com>
2019-01-11 00:25:33 +0400

    added banner, league logo. tornaments page theming

--- MyFootballMvc/MyFootballMvc/Controllers/LeagueController.cs
+++ MyFootballMvc/MyFootballMvc/Controllers/LeagueController.cs
@@ -13,70 +13,70 @@ namespace MyFootballMvc.Controllers
 
     public IActionResult Fixtures(string tournamentId)
     {
-      ViewBag.Current = "fixtures";
       var leagueViewModel = new LeagueViewModel(tournamentId);
+      leagueViewModel.ActiveTab = "fixtures";
       return View("~/Views/League/Fixtures.cshtml", leagueViewModel);
     }
     public IActionResult Results(string tournamentId)
     {
-      ViewBag.Current = "results";
       var leagueViewModel = new LeagueViewModel(tournamentId);
+      leagueViewModel.ActiveTab = "results";
       return View("~/Views/League/Results.cshtml", leagueViewModel);
     }
 
     public IActionResult Tables(string tournamentId)
     {
-      ViewBag.Current = "tables";
       var leagueViewModel = new LeagueViewModel(tournamentId);
+      leagueViewModel.ActiveTab = "tables";
       return View("~/Views/League/Tables.cshtml", leagueViewModel);
     }
 
     public IActionResult Clubs(string tournamentId)
     {
-      ViewBag.Current = "clubs";
       var leagueViewModel = new LeagueViewModel(tournamentId);
+      leagueViewModel.ActiveTab = "clubs";
       return View("~/Views/League/Clubs.cshtml", leagueViewModel);
     }
 
     public IActionResult Players(string tournamentId)
     {
-      ViewBag.Current = "players";
       var leagueViewModel = new LeagueViewModel(tournamentId);
+      leagueViewModel.ActiveTab = "players";
       return View("~/Views/League/Players.cshtml", leagueViewModel);
     }
 
     public IActionResult Managers(string tournamentId)
     {
-      ViewBag.Current = "managers";
       var leagueViewModel = new LeagueViewModel(tournamentId);
+      leagueViewModel.ActiveTab = "managers";
       return View("~/Views/League/Managers.cshtml", leagueViewModel);
     }
 
     public IActionResult News(string tournamentId)
     {
-      ViewBag.Current = "news";
       var leagueViewModel = new LeagueViewModel(tournamentId);
+      leagueViewModel.ActiveTab = "news";
       return View("~/Views/League/News.cshtml", leagueViewModel);
     }
 
     public IActionResult Social(string tournamentId)
     {
-      ViewBag.Current = "social";
       var leagueViewModel = new LeagueViewModel(tournamentId);
+      leagueViewModel.ActiveTab = "social";
       return View("~/Views/League/Social.cshtml", leagueViewModel);
     }
 
     public IActionResult History(string tournamentId)
     {
-      ViewBag.Current = "history";
       var leagueViewModel = new LeagueViewModel(tournamentId);
+      leagueViewModel.ActiveTab = "history";
       return View("~/Views/League/History.cshtml", leagueViewModel);
     }
 
     public IActionResult Referees(string tournamentId)
     {
-      ViewBag.Current = "referees";
       var leagueViewModel = new LeagueViewModel(tournamentId);
+      leagueViewModel.ActiveTab = "referees";
       return View("~/Views/League/Referees.cshtml", leagueViewModel);
     }
   }
--- MyFootballMvc/MyFootballMvc/ViewModels/LeagueViewModel.cs
+++ MyFootballMvc/MyFootballMvc/ViewModels/LeagueViewModel.cs
@@ -8,15 +8,19 @@ using RestSharp;
 
 namespace MyFootballMvc.ViewModels
 {
-    public class LeagueViewModel : TournamentViewModel
-    {
-        public LeagueViewModel(string leagueId)
-        {
-            var client = new RestClient($@"https://localhost:44350/api/Tournament/{leagueId}");
-            var request = new RestRequest(Method.GET);
-            var response = client.Execute(request);
+  public class LeagueViewModel : TournamentViewModel
+  {
+    public string ActiveTab { get; set; }
+    public byte[] Logo { get; set; }
+    public byte[] Banner { get; set; }
 
-            Tournament = JsonConvert.DeserializeObject<Tournament>(response.Content);
-        }
+    public LeagueViewModel(string leagueId)
+    {
+      var client = new RestClient($@"https://localhost:44350/api/Tournament/{leagueId}");
+      var request = new RestRequest(Method.GET);
+      var response = client.Execute(request);
+
+      Tournament = JsonConvert.DeserializeObject<Tournament>(response.Content);
     }
+  }
 }
