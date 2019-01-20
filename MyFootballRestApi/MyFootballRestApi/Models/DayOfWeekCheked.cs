2d71eb3657aaafdae8333017ede33be0dbceb7b5
Artur Aslanian <aslanianartur1@gmail.com>
2019-01-20 12:54:25 +0400

    fix

---
+++ MyFootballRestApi/MyFootballRestApi/Models/DayOfWeekCheked.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFootballRestApi.Models
+{
+    public class DayOfWeekCheked
+    {
+        private DayOfWeek _dayOfWeek;
+        private bool _isCheked;
+        public DayOfWeek DayOfWeek
+        {
+            get { return _dayOfWeek; }
+            set { SetProperty(ref _dayOfWeek, value); }
+        }
+        public bool IsCheked
+        {
+            get { return _isCheked; }
+            set { SetProperty(ref _isCheked, value); }
+        }
+    }
+}
--- MyFootballRestApi/MyFootballRestApi/Models/League.cs
+++ MyFootballRestApi/MyFootballRestApi/Models/League.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace MyFootballRestApi.Models
 {
     public class League:EntityBase<League>
-    {        
+    {
         public List<Team> Teams { get; set; }
         public int CountOfMatches { get; set; }
         public DateTime StartDate { get; set; }
@@ -13,10 +13,14 @@ namespace MyFootballRestApi.Models
         public List<Rule> Rules { get; set; }
         public List<Pause> Pauses { get; set; }
         public Tournament Tournament { get; set; }
-        public int PlayersCount { get; set; }
         public List<Tour> Tour { get; set; }
-        public ImageLink Logo { get; set; }
         public LeagueResultTable ResultTable { get; set; }
+        public int OneHalfTime { get; set; }
+        public int BreakTime { get; set; }
+        public int PlayersCount { get; set; }
+        public int YelloCardsToDisqualification { get; set; }
+        private int[,] Play;
+        private int l;
 
     }
 }
