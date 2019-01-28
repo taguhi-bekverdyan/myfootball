21518fb492f75e0ff41c00d8e16ec5a885e11b3e
Taguhi Bekverdyan <tagnvard@gmail.com>
2019-01-28 04:07:27 +0400

    adds players page

---
+++ MyFootballMvc/MyFootballMvc/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MyFootballMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFootballMvc.ViewModels
+{
+    public class PlayerViewModel:LayoutViewModel
+    {
+        public List<Player> Players { get; set; }
+        public Player Player { get; set; }
+
+        public PlayerViewModel()
+        {
+
+        }
+
+        public PlayerViewModel(string token, string id) : base(token, id)
+        {
+
+        }
+    }
+}
