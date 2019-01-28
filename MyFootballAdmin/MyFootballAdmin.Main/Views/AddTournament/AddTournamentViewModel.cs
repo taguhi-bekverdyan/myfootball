f9b343075e0a9ee4419984d914adaf38ab51f531
Nare Matevosyan <nare.matevosyan@optym.com>
2019-01-28 14:19:02 +0400

    Unified tournament creation view.

--- MyFootballAdmin/MyFootballAdmin.Main/Views/AddTournament/AddTournamentViewModel.cs
+++ MyFootballAdmin/MyFootballAdmin.Main/Views/AddTournament/AddTournamentViewModel.cs
@@ -18,6 +18,10 @@ using System.Drawing;
 using System.IO;
 using System.Drawing.Imaging;
 using MyFootballAdmin.Data.Models;
+using System.Collections.ObjectModel;
+using MyFootballAdmin.Data.Services.TournamentService;
+using System.Runtime.Serialization.Json;
+using MyFootballAdmin.Data.Services.LeagueService;
 
 namespace MyFootballAdmin.Main.Views.AddTournament
 {
@@ -28,13 +32,36 @@ namespace MyFootballAdmin.Main.Views.AddTournament
         private readonly IEventAggregator _eventAggregator;
         private readonly INotificationService _notificationService;
         private readonly IRegionManager _regionManager;
+        private readonly ITournamentService _tournamentService;
+        private readonly ILeagueService _leagueService;
 
-        public AddTournamentViewModel(IShellService shellService, IEventAggregator eventAggregator, INotificationService notificationService, IRegionManager regionManager)
+        public AddTournamentViewModel(IShellService shellService, 
+                                      IEventAggregator eventAggregator, 
+                                      INotificationService notificationService, 
+                                      IRegionManager regionManager,
+                                      ITournamentService tournamentService,
+                                      ILeagueService leagueService)
         {
             _shellService = shellService;
             _eventAggregator = eventAggregator;
             _notificationService = notificationService;
             _regionManager = regionManager;
+            _tournamentService = tournamentService;
+            _leagueService = leagueService;
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            DaysOfWeek = new ObservableCollection<DayOfWeekCheked>();
+
+            for (int dayNumber = 0; dayNumber < 7; dayNumber++)
+            {
+                DaysOfWeek.Add(new DayOfWeekCheked() { DayOfWeek = (DayOfWeek)dayNumber });
+            }
+
+            Pauses = new ObservableCollection<Pause>() { new Pause() };
         }
 
         #region Types
@@ -53,9 +80,7 @@ namespace MyFootballAdmin.Main.Views.AddTournament
         {
             get { return _name; }
             set { SetProperty(ref _name, value); }
-        }
-
-     
+        }     
 
         private int _priority;
 
@@ -80,6 +105,69 @@ namespace MyFootballAdmin.Main.Views.AddTournament
             get { return _tournamentType; }
             set { SetProperty(ref _tournamentType, value); }
         }
+
+        private DateTime? startDate;
+
+        public DateTime? StartDate
+        {
+            get { return startDate; }
+            set { SetProperty(ref startDate, value); }
+        }
+
+        private DateTime? endDate;
+
+        public DateTime? EndDate
+        {
+            get { return endDate; }
+            set { SetProperty(ref endDate, value); }
+        }
+
+        public ObservableCollection<DayOfWeekCheked> DaysOfWeek { get; set; }
+
+        public ObservableCollection<Pause> Pauses { get; set; }
+
+        public ObservableCollection<Rule> Rules { get; set; }
+
+        private int oneHalfTime;
+
+        public int OneHalfTime
+        {
+            get { return oneHalfTime; }
+            set { SetProperty(ref oneHalfTime, value); }
+        }
+
+        private int breakTime;
+
+        public int BreakTime
+        {
+            get { return breakTime; }
+            set { SetProperty(ref breakTime, value); }
+        }
+
+        private int playersCount;
+
+        public int PlayersCount
+        {
+            get { return playersCount; }
+            set { SetProperty(ref playersCount, value); }
+        }
+
+        private int yelloCardsToDisqualification;
+
+        public int YelloCardsToDisqualification
+        {
+            get { return yelloCardsToDisqualification; }
+            set { SetProperty(ref yelloCardsToDisqualification, value); }
+        }
+
+        private int countOfMatches;
+
+        public int CountOfMatches
+        {
+            get { return countOfMatches; }
+            set { SetProperty(ref countOfMatches, value); }
+        }
+
         #endregion
 
 
@@ -87,27 +175,82 @@ namespace MyFootballAdmin.Main.Views.AddTournament
 
         #region Commands
 
-        private DelegateCommand _nextCommand;
+        private DelegateCommand<string> _selectTypeCommand;
 
-        public DelegateCommand NextCommand => _nextCommand ?? (_nextCommand = new DelegateCommand(NextCommandAction));
+        public DelegateCommand<string> SelectTypeCommand => _selectTypeCommand ?? (_selectTypeCommand = new DelegateCommand<string>(SelectTypeCommandAction));
 
+        private void SelectTypeCommandAction(string type)
+        {
+            Enum.TryParse(type, out TournamentType tournamentType);
+            TournamentType = tournamentType;
+        }
 
-        public void NextCommandAction()
+        private DelegateCommand addPauseCommand;
+
+        public DelegateCommand AddPauseCommand => addPauseCommand ?? (addPauseCommand = new DelegateCommand(AddPauseCommandAction));
+
+        private void AddPauseCommandAction()
+        {
+            Pauses.Add(new Pause());
+        }
+
+        private DelegateCommand<object> deletePauseCommand;
+
+        public DelegateCommand<object> DeletePauseCommand => deletePauseCommand ?? (deletePauseCommand = new DelegateCommand<object>(DeletePauseCommandAction));
+
+        private void DeletePauseCommandAction(object pause)
+        {
+            Pauses.Remove(pause as Pause);
+        }
+
+        private DelegateCommand cancelCommand;
+
+        public DelegateCommand CancelCommand => cancelCommand ?? (cancelCommand = new DelegateCommand(CancelCommandAction));
+
+        private void CancelCommandAction()
+        {
+            _regionManager.RequestNavigate(RegionNames.BesidesToolBarRegion, typeof(BesidesToolBarView).FullName);
+        }
+
+        private DelegateCommand saveCommand;
+
+        public DelegateCommand SaveCommand => saveCommand ?? (saveCommand = new DelegateCommand(SaveCommandAction));
+
+        private async void SaveCommandAction()
         {
             Tournament Tournament = new Tournament();
             Tournament.Name = Name;
             Tournament.Priority = Priority;
             Tournament.TournamentType = TournamentType;
-            //Tournament.image = GetBytesFromImage(ImagePath);
-            NavigationParameters param;
-            param = new NavigationParameters { { "request", Tournament} };
-            if (TournamentType.Equals(TournamentType.League))
+            //todo: add logo uploading
+
+            var existingTournaments = await _tournamentService.FindAll();
+
+            if(existingTournaments.Any(t => t.Name == Tournament.Name))
             {
-                _regionManager.RequestNavigate(RegionNames.AddTournamentRegion, typeof(AddLeagueView).FullName, param);
+                return;
+            }
+
+            if (TournamentType == TournamentType.League)
+            {
+                var League = new League();
+                League.Tournament = Tournament;
+                League.MatchDays = DaysOfWeek.Where(d => d.IsCheked).Select(d => d.DayOfWeek).ToList();
+                League.Pauses = Pauses.ToList();
+
+                await _leagueService.Create(League);
+
+                DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(League));
+                using (FileStream fs = new FileStream("league.json", FileMode.OpenOrCreate))
+                {
+                    jsonFormatter.WriteObject(fs, League);
+                }
+
+                _regionManager.RequestNavigate(RegionNames.BesidesToolBarRegion, typeof(BesidesToolBarView).FullName);
             }
             else
             {
-                _regionManager.RequestNavigate(RegionNames.AddTournamentRegion, typeof(AddCupView).FullName, param);
+                //todo: handle cup creation
             }
         }
 
@@ -168,6 +311,5 @@ namespace MyFootballAdmin.Main.Views.AddTournament
         }
 
         #endregion
-
     }
 }
