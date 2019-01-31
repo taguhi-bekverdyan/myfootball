7af77c7f440b589277b8e70950d25a13e7333d72
Artur Aslanian <aslanianartur1@gmail.com>
2019-02-01 00:28:21 +0400

    fixtures,results,league_views

---
+++ MyFootballMvc/MyFootballMvc/ViewModels/FixtureViewItem.cs
@@ -0,0 +1,14 @@
+﻿using MyFootballMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFootballMvc.ViewModels
+{
+    public class FixtureViewItem
+    {
+        public List<Tour> Tours { get; set; }
+        public bool IsGenerated { get; set; }
+    }
+}
