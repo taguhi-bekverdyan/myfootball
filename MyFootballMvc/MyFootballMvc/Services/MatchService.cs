1e6f609aafb325d47b50907f52df244744965e69
Artur Aslanian <aslanianartur1@gmail.com>
2019-02-02 12:17:03 +0400

    fix

---
+++ MyFootballMvc/MyFootballMvc/Services/MatchService.cs
@@ -0,0 +1,32 @@
+﻿using MyFootballMvc.Models;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFootballMvc.Services
+{
+    public class MatchService
+    {
+        private const string Endpoint = "https://localhost:44350/api";
+        private readonly RestClient _client;
+        public MatchService()
+        {
+            _client = new RestClient(Endpoint);
+        }
+
+        public async Task<Match> FindMatchById(string accessToken, string id)
+        {
+            var request = new RestRequest("match/{id}", Method.GET);
+            request.AddHeader("authorization", $"Bearer {accessToken}");
+            request.AddUrlSegment("id", id);
+
+            IRestResponse response = await _client.ExecuteTaskAsync(request);
+
+            Match match = JsonConvert.DeserializeObject<Match>(response.Content);
+            return match;
+        }
+    }
+}
---
+++ MyFootballMvc/MyFootballMvc/ViewModels/MatchViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MyFootballMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFootballMvc.ViewModels
+{
+    public class MatchViewModel:LayoutViewModel
+    {
+        public Match Match { get; set; } = new Match();
+        public Referee Referee { get; set; } = new Referee();
+
+        public MatchViewModel(string token, string id) : base(token, id)
+        {
+
+        }
+
+        public MatchViewModel()
+        {
+
+        }
+    }
+}
