66566f68c77fa178ee5c8c67df1aa51456192cc5
Karen Pirumyan <k.pirumyan@gmail.com>
2019-02-03 14:04:58 +0400

    #29 changes datetime to current date for create user form

--- MyFootballMvc/MyFootballMvc/Models/User.cs
+++ MyFootballMvc/MyFootballMvc/Models/User.cs
@@ -6,17 +6,19 @@ using System.Threading.Tasks;
 
 namespace MyFootballMvc.Models
 {
-    
-    public class User : EntityBase<User>
-    {
-        [MaxLength(80), Required, Display(Name = "First Name")]
-        public string FirstName { get; set; }
-        [MaxLength(80), Required, Display(Name = "Last Name")]
-        public string LastName { get; set; }
-        [Required, Display(Name = "Date of birth")]
-        public DateTime Birthdate { get; set; }
 
-        public Guid FavoriteTeamId { get; set; }
+  public class User : EntityBase<User>
+  {
+    [MaxLength(80), Required, Display(Name = "First Name")]
+    public string FirstName { get; set; }
 
-    }
+    [MaxLength(80), Required, Display(Name = "Last Name")]
+    public string LastName { get; set; }
+
+    [Required, Display(Name = "Date of birth")]
+    public DateTime Birthdate { get; set; } = DateTime.Now;
+
+    public Guid FavoriteTeamId { get; set; }
+
+  }
 }
