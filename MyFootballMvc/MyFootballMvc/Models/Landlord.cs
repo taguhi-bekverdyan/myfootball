b83251e34566f8184cbd3f153155a3fc5aabd2d6
Karen Pirumyan <k.pirumyan@gmail.com>
2019-02-10 14:00:09 +0400

    #20 landlord phone number

--- MyFootballMvc/MyFootballMvc/Models/Landlord.cs
+++ MyFootballMvc/MyFootballMvc/Models/Landlord.cs
@@ -6,6 +6,13 @@ namespace MyFootballMvc.Models
   {
     [Required(ErrorMessage = "Please enter your Organization name")]
     public string Organization { get; set; }
+
+    [Required(ErrorMessage = "You must provide a phone number")]
+    [Display(Name = "Phone")]
+    [DataType(DataType.PhoneNumber)]
+    [RegularExpression(@"^\(?[-. ]?([0-9]{2})[-. ]?([0-9]{6})$", ErrorMessage = "Not a valid phone number")]
+    public string PhoneNumber { get; set; }
+
     public User User { get; set; }
   }
 }
